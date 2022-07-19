79ba414bc03db037aa8fa343e5af201651395484
mdminhaziftekhar <mdminhaziftekhar@gmail.com>
2022-07-19 22:08:49 +0600

    feat: Added Main Data Access Object (MainDAO)

---
+++ app/src/main/java/com/example/todolistapp/Database/MainDAO.java
@@ -0,0 +1,28 @@
+package com.example.todolistapp.Database;
+
+import static androidx.room.OnConflictStrategy.REPLACE;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import com.example.todolistapp.Models.Notes;
+
+import java.util.List;
+
+@Dao
+public interface MainDAO {
+
+    @Insert(onConflict = REPLACE)
+    void insert(Notes notes);
+
+    @Query("SELECT * FROM notes ORDER BY id DESC")
+    List<Notes> getAll();
+
+    @Query("UPDATE notes SET title = :title, notes = :notes WHERE ID = :id")
+    void update(int id, String title, String notes);
+
+    @Delete
+    void delete(Notes notes);
+}
